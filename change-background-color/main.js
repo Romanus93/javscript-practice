532d8fb87cb19b72e23a6660d56ea9b34f9b4f54
romanus93 <obj.romanus@gmail.com>
2021-06-20 18:41:06 +0900

    코드 수정

--- change-background-color/main.js
+++ change-background-color/main.js
@@ -1,29 +1,20 @@
 const button = document.querySelector("button");
 const body = document.querySelector("body");
 const colorArr = [ 'red' , 'yellow' , 'orange', 'black' ];
-console.log(colorArr);
-// body.style.backgroundColor = 'violet'
-const bgColor = body.style.backgroundColor;
 const bgColorArr = [];
 
 function removeCurVal( curVal ) {
-  //중복되었을 때
-  console.log('중복중복중복중복')
-  const clonedColorArr = colorArr;
-  console.log(clonedColorArr);
+  // 값이 중복되었을 때
   // 중복되는 색을 뺀 새로운 배열
-  const filteredColor = clonedColorArr.filter((element) => element !== curVal);
-  console.log(filteredColor);
-  let reColorIndex = parseInt(Math.random()*(filteredColor.length)) - 1;
-  // Math.random()이 0이 나와 reColorIndex가 -1이 된 경우를 대비
-  (reColorIndex === -1 )&&(reColorIndex = 0);
-  console.log('removeCurVal 종료')
-  return filteredColor[reColorIndex];
+  const filteredColorArr = colorArr.filter((element) => element !== curVal);
+  let filteredColorIndex = parseInt(Math.random()*(filteredColorArr.length)) - 1;
+  // Math.random()이 0이 나와 filteredColorIndex가 -1이 된 경우를 대비
+  (filteredColorIndex === -1 )&&(filteredColorIndex = 0);
+  return filteredColorArr[filteredColorIndex];
 }
 
-function changeBG () {
+function changeBgColor () {
   const colorIndex = parseInt(Math.random()*(colorArr.length));
-  console.log('조건문',new Date().getMilliseconds());
   
   bgColorArr.push(colorArr[colorIndex]);
   const preVal = bgColorArr[bgColorArr.length-2];
@@ -36,13 +27,9 @@ function changeBG () {
     const noOverlapColor = removeCurVal(curVal)
     bgColorArr.push(noOverlapColor);
     body.style.backgroundColor = noOverlapColor;
-    console.log('bgColor 조건문 종료');
     return
   };
-  console.log('현',curVal,'배열',colorArr[colorIndex]);
   body.style.backgroundColor = colorArr[colorIndex];
-  console.log('전체 종료');
 }
 
-button.addEventListener('click', changeBG )
-
+button.addEventListener('click', changeBgColor );
