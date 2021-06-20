f90fd3b64f2006d5221943a30f9133ba98d576d4
romanus93 <obj.romanus@gmail.com>
2021-06-20 18:29:32 +0900

    질문, 새로 복사해서 쓰는게 맞는지.

--- change-background-color/main.js
+++ change-background-color/main.js
@@ -1,36 +1,48 @@
 const button = document.querySelector("button");
 const body = document.querySelector("body");
-//const color = [ 'red' , 'yellow' , 'orange', 'black' ];
-const color = [ 'red' , 'yellow'];
+const colorArr = [ 'red' , 'yellow' , 'orange', 'black' ];
+console.log(colorArr);
 // body.style.backgroundColor = 'violet'
 const bgColor = body.style.backgroundColor;
 const bgColorArr = [];
 
-button.addEventListener('click', changeBG )
-function changeBG () {
-  const colorIndex = parseInt(Math.random()*(color.length));
-  console.log('조건문',new Date().getMilliseconds());
-  
-  bgColorArr.push(color[colorIndex]);
-  let preVal = bgColorArr[bgColorArr.length-2];
-  let curVal = bgColorArr[bgColorArr.length-1];
-  console.log(preVal,curVal);
-
-  if(bgColorArr.length > 1) {
-    console.log( `배열 - ${bgColorArr}, 배열 길이 - ${bgColorArr.length}, 이전 값 - ${preVal}, 현재 값 ${curVal} , 이전 현재  값 비교 ${preVal===curVal}` );
-    (preVal===curVal)&&(changeBG());
-    // body.style.backgroundColor = curVal;
-    // return
-  } 
-  console.log('현',curVal,'배열',color[colorIndex]);
-  body.style.backgroundColor = color[colorIndex];
-  console.log('종료');
+function removeCurVal( curVal ) {
+  //중복되었을 때
+  console.log('중복중복중복중복')
+  const clonedColorArr = colorArr;
+  console.log(clonedColorArr);
+  // 중복되는 색을 뺀 새로운 배열
+  const filteredColor = clonedColorArr.filter((element) => element !== curVal);
+  console.log(filteredColor);
+  let reColorIndex = parseInt(Math.random()*(filteredColor.length)) - 1;
+  // Math.random()이 0이 나와 reColorIndex가 -1이 된 경우를 대비
+  (reColorIndex === -1 )&&(reColorIndex = 0);
+  console.log('removeCurVal 종료')
+  return filteredColor[reColorIndex];
 }
 
-// changeBg는 값을 받아서 
+function changeBG () {
+  const colorIndex = parseInt(Math.random()*(colorArr.length));
+  console.log('조건문',new Date().getMilliseconds());
+  
+  bgColorArr.push(colorArr[colorIndex]);
+  const preVal = bgColorArr[bgColorArr.length-2];
+  const curVal = bgColorArr[bgColorArr.length-1];
+  console.log(preVal,curVal);
+  
+  if(bgColorArr.length > 1 && preVal===curVal) {
+    console.log( `배열 - ${bgColorArr}, 배열 길이 - ${bgColorArr.length}, 이전 값 - ${preVal}, 현재 값 ${curVal} , 이전 현재  값 비교 ${preVal===curVal}` );
+    // 중복되지 않는 값.
+    const noOverlapColor = removeCurVal(curVal)
+    bgColorArr.push(noOverlapColor);
+    body.style.backgroundColor = noOverlapColor;
+    console.log('bgColor 조건문 종료');
+    return
+  };
+  console.log('현',curVal,'배열',colorArr[colorIndex]);
+  body.style.backgroundColor = colorArr[colorIndex];
+  console.log('전체 종료');
+}
 
-// index가 0인 경우 / 아닌 경우 
-
-// 값을 넣은 배열에서 i-1, i 비교
-
+button.addEventListener('click', changeBG )
 
