2deea76b15d3736e4044b0c318b6a5600d830204
romanus93 <obj.romanus@gmail.com>
2021-06-21 13:43:26 +0900

    네이밍 수정, removeCurVal() -> removeOverlapColor() , 주석 수정

--- change-background-color/main.js
+++ change-background-color/main.js
@@ -3,8 +3,7 @@ const body = document.querySelector("body");
 const colorArr = [ 'red' , 'yellow' , 'orange', 'black' ];
 const bgColorArr = [];
 
-function removeCurVal( curVal ) {
-  // 값이 중복되었을 때
+function removeOverlapColor( curVal ) {
   // 중복되는 색을 뺀 새로운 배열
   const filteredColorArr = colorArr.filter((element) => element !== curVal);
   let filteredColorIndex = parseInt(Math.random()*(filteredColorArr.length)) - 1;
@@ -15,16 +14,16 @@ function removeCurVal( curVal ) {
 
 function changeBgColor () {
   const colorIndex = parseInt(Math.random()*(colorArr.length));
-  
   bgColorArr.push(colorArr[colorIndex]);
   const preVal = bgColorArr[bgColorArr.length-2];
   const curVal = bgColorArr[bgColorArr.length-1];
   console.log(preVal,curVal);
   
+  // 배경색이 동일한 경우
   if(bgColorArr.length > 1 && preVal===curVal) {
     console.log( `배열 - ${bgColorArr}, 배열 길이 - ${bgColorArr.length}, 이전 값 - ${preVal}, 현재 값 ${curVal} , 이전 현재  값 비교 ${preVal===curVal}` );
-    // 중복되지 않는 값.
-    const noOverlapColor = removeCurVal(curVal)
+    //중복되지 않는 값.
+    const noOverlapColor = removeOverlapColor(curVal)
     bgColorArr.push(noOverlapColor);
     body.style.backgroundColor = noOverlapColor;
     return
